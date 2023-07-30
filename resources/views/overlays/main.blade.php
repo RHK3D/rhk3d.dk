256478ff2823a6d7aa37301d3d7ee79d71524565
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2023-07-30 18:39:27 +0200

    Update main.blade.php

--- resources/views/overlays/main.blade.php
+++ resources/views/overlays/main.blade.php
@@ -10,23 +10,25 @@
     @vite('resources/js/app.js')
     @vite('resources/js/overlay.js')
 </head>
-<body>
-    <div class="absolute bottom-4 left-4 flex flex-row text-red-600">
-        <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2">
-            <h1 class="text-lg font-medium">Lokal tid</h1>
-            <p id="clock" class="text-2xl font-semibold">00:00</p>
-        </div>
-        <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2">
-            <h1 class="text-lg font-medium">Lokation</h1>
-            <p id="city" class="text-2xl font-semibold">...</p>
-        </div>
-        <!--div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2">
-            <h1 class="text-lg font-medium">Højde over havet</h1>
-            <p id="altitude" class="text-2xl font-semibold"></p>
-        </div-->
-        <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2" id="speedometer-container">
-            <h1 class="text-lg font-medium">Hastighed (BETA)</h1>
-            <p id="speedometer" class="text-2xl font-semibold"></p>
+<body class="h-screen">
+    <div class="absolute right-0 top-full left-1/2 transform -translate-x-1/2 -translate-y-full text-red-600">
+        <div class="flex flex-row justify-center mb-4">
+            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2">
+                <h1 class="text-lg font-medium">Lokal tid</h1>
+                <p id="clock" class="text-2xl font-semibold">00:00</p>
+            </div>
+            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2">
+                <h1 class="text-lg font-medium">Lokation</h1>
+                <p id="city" class="text-2xl font-semibold">...</p>
+            </div>
+            <!--div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2">
+                <h1 class="text-lg font-medium">Højde over havet</h1>
+                <p id="altitude" class="text-2xl font-semibold"></p>
+            </div-->
+            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2" id="speedometer-container">
+                <h1 class="text-lg font-medium">Hastighed (BETA)</h1>
+                <p id="speedometer" class="text-2xl font-semibold"></p>
+            </div>
         </div>
     </div>
     <div class="absolute bottom-4 right-4 p-2 flex flex-col rounded-md">
