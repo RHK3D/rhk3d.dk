dae5e63b3bc62f9e0af6a394d344d441ba65e960
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2023-10-19 10:41:47 +0200

    MAGIC

--- resources/views/overlays/main.blade.php
+++ resources/views/overlays/main.blade.php
@@ -4,35 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Preview overlay - RHK3D</title>
     <script src="https://unpkg.com/@mapbox/mapbox-sdk/umd/mapbox-sdk.min.js"></script>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     @vite('resources/js/overlay.js')
 </head>
 <body>
-    <div class="flex justify-center">
-        <div class="flex flex-row text-red-600 mt-4">
-            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2">
-                <h1 class="text-lg font-medium">Lokal tid</h1>
-                <p id="clock" class="text-2xl font-semibold">00:00</p>
-            </div>
-            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2">
-                <h1 class="text-lg font-medium">Lokation</h1>
-                <p id="city" class="text-2xl font-semibold">...</p>
-            </div>
-            <!--div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2">
-                <h1 class="text-lg font-medium">Højde over havet</h1>
-                <p id="altitude" class="text-2xl font-semibold"></p>
-            </div-->
-            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2" id="speedometer-container">
-                <h1 class="text-lg font-medium">Hastighed (BETA)</h1>
-                <p id="speedometer" class="text-2xl font-semibold"></p>
+
+    <nav class="bg-black border-b-4 border-sky-600">
+    <div class="mx-auto">
+        <div class="relative flex h-10 items-center justify-between">
+        <div class="flex flex-1 items-center justify-center sm:items-stretch">
+            <div class="flex flex-shrink-0 items-center">
+                <div class="flex space-x-2">
+                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                    <a href="#" class="text-white text-md font-medium px-3">Lokal tid: <span id="clock" class="font-normal">00:00</span></a>
+                    <a href="#" class="text-white text-md font-medium px-3">Lokation: <span id="city" class="font-normal">...</span></a>
+                    <a href="#" class="text-white text-md font-medium px-3">Hastighed: <span id="speedometer" class="font-normal">0 km/t</span></a>
+                </div>
             </div>
         </div>
     </div>
-    <div class="hidden absolute bottom-4 right-4 p-2 flex flex-col rounded-md">
-        <span class="text-5xl text-blue-800 font-semibold">RHK3D</span>
-    </div>
+    </nav>
+
 </body>
 </html>
