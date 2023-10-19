323494476a340b7e33721271ca4a623fc3e1f7a5
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2023-10-19 09:50:12 +0200

    Remove logo

--- resources/views/overlays/preview.blade.php
+++ resources/views/overlays/preview.blade.php
@@ -17,18 +17,16 @@
         <div class="relative flex h-16 items-center justify-between">
         <div class="flex flex-1 items-center justify-center sm:items-stretch">
             <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
-            </div>
-            <div class="hidden sm:ml-6 sm:block">
-            <div class="flex space-x-2">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="#" class="text-white text-sm font-medium px-3 py-2">Lokal tid: <span id="clock" class="font-normal">00:00</span></a>
-                <a href="#" class="text-white text-sm font-medium px-3 py-2">Lokation: <span id="city" class="font-normal">...</span></a>
-                <a href="#" class="text-white text-sm font-medium px-3 py-2">Hastighed: <span id="speedometer" class="font-normal">79 km/t</span></a>
-            </div>
+                <div class="flex space-x-2">
+                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                    <a href="#" class="text-white text-sm font-medium px-3 py-2">Lokal tid: <span id="clock" class="font-normal">00:00</span></a>
+                    <a href="#" class="text-white text-sm font-medium px-3 py-2">Lokation: <span id="city" class="font-normal">...</span></a>
+                    <a href="#" class="text-white text-sm font-medium px-3 py-2">Hastighed: <span id="speedometer" class="font-normal">79 km/t</span></a>
+                </div>
             </div>
         </div>
     </div>
     </nav>
+
 </body>
 </html>
