c6283abe04c97adbe5861442ee3e8d149a13e0ce
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2023-10-19 09:43:55 +0200

    Test

--- resources/views/overlays/preview.blade.php
+++ resources/views/overlays/preview.blade.php
@@ -22,33 +22,13 @@
             <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-2">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="#" class="text-white text-sm font-medium px-3 py-2">Lokal tid: <span class="font-normal">12:34</span></a>
-                <a href="#" class="text-white text-sm font-medium px-3 py-2">Lokation: <span class="font-normal">Skanderborg, DK</span></a>
-                <a href="#" class="text-white text-sm font-medium px-3 py-2">Hastighed: <span class="font-normal">79 km/t</span></a>
+                <a href="#" class="text-white text-sm font-medium px-3 py-2">Lokal tid: <span id="clock" class="font-normal">00:00</span></a>
+                <a href="#" class="text-white text-sm font-medium px-3 py-2">Lokation: <span id="city" class="font-normal">...</span></a>
+                <a href="#" class="text-white text-sm font-medium px-3 py-2">Hastighed: <span id="speedometer" class="font-normal">79 km/t</span></a>
             </div>
             </div>
         </div>
     </div>
     </nav>
-
-    <div class="flex justify-center">
-        <div class="flex flex-row text-red-600 mt-4">
-            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2">
-                <h1 class="text-lg font-medium">Lokal tid</h1>
-                <p id="clock" class="text-2xl font-semibold">00:00</p>
-            </div>
-            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2">
-                <h1 class="text-lg font-medium">Lokation</h1>
-                <p id="city" class="text-2xl font-semibold">...</p>
-            </div>
-            <div class="bg-black backdrop-blur-sm rounded-md bg-opacity-90 p-2 ml-2" id="speedometer-container">
-                <h1 class="text-lg font-medium">Hastighed (BETA)</h1>
-                <p id="speedometer" class="text-2xl font-semibold"></p>
-            </div>
-        </div>
-    </div>
-    <div class="hidden absolute bottom-4 right-4 p-2 flex flex-col rounded-md">
-        <span class="text-5xl text-blue-800 font-semibold">RHK3D</span>
-    </div>
 </body>
 </html>
