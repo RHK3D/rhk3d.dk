10ce45dc5e88976e41e7d45c479dc0f44a5d11b9
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2025-01-15 14:20:35 +0200

    Update overlay.js

--- resources/js/overlay.js
+++ resources/js/overlay.js
@@ -15,7 +15,7 @@ const interval = setInterval(function() {
     const today = new Date();
     let clock = '--:--';
     let minutes = today.getMinutes();
-    let hours = today.getHours();
+    let hours = today.getHours()+1;
 
     minutes = minutes < 10 ? "0" + minutes : minutes;
     hours = hours < 10 ? "0" + hours : hours;
